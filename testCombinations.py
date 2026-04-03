8866c8856885ae88644c39a1e33e18d7bfe4088f
misProyectosPropios <inakifrutos@outlook.com>
2026-04-03 08:13:43 -0300

    Test parenthesis requirements

--- testCombinations.py
+++ testCombinations.py
@@ -1,8 +1,8 @@
 import unittest
 from decider import * 
-from solver import *
 
-class TestStringMethods(unittest.TestCase):
+
+class TestCombinationsMethods(unittest.TestCase):
 
     def test_constant(self):
         const: Constant = Constant(1)
@@ -82,6 +82,60 @@ class TestStringMethods(unittest.TestCase):
         res: set = division.generateAllCombinations()
 
         self.assertEqual(len(res), 4 ** 2)
+
+class TestShapesMethod(unittest.TestCase):
+    def test_noConstant(self):
+        res: list[AST] = generateAllGraphs([])
+        self.assertEqual(len(res), 0)
+
+    def test_oneConstant(self):
+        res: list[AST] = generateAllGraphs([Constant(1)])
+        self.assertEqual(len(res), 1)
+
+    def test_twoConstant(self):
+        res: list[AST] = generateAllGraphs([Constant(1), Constant(2)])
+        self.assertEqual(len(res), 4)
+
+    def test_threeConstant(self):
+        lista_const = [Constant(1), Constant(2), Constant(3)]
+        res: list[AST] = generateAllGraphs(lista_const)
+        self.assertEqual(len(res), 32)
+
+class TestParenthesisNeeded(unittest.TestCase):
+    def test_constant(self):
+        value: AST = Constant(2)
+        res: int = value.numberOfParenthesisNeeded()
+        self.assertEqual(res, 0)
+
+    def test_addition(self):
+        value: AST = Addition(Constant(2), Constant(3))
+        res: int = value.numberOfParenthesisNeeded()
+        self.assertEqual(res, 0)
+
+    def test_multiplication(self):
+        value: AST = Multiplication(Constant(2), Constant(3))
+        res: int = value.numberOfParenthesisNeeded()
+        self.assertEqual(res, 0)
+
+    def test_MultipleAddition(self):
+        value: AST = Addition(Constant(2), Addition(Constant(3), Constant(5)))
+        res: int = value.numberOfParenthesisNeeded()
+        self.assertEqual(res, 0)
+
+    def test_MultipleMultiplication(self):
+        value: AST = Multiplication(Constant(2), Multiplication(Constant(3), Constant(5)))
+        res: int = value.numberOfParenthesisNeeded()
+        self.assertEqual(res, 0)
+
+    def test_multiplicationOfAddition(self):
+        value: AST = Multiplication(Constant(2), Addition(Constant(3), Constant(5)))
+        res: int = value.numberOfParenthesisNeeded()
+        self.assertEqual(res, 1)
+
+    def test_multiplicationBetweenTwoAdditions(self):
+        value: AST = Multiplication(Addition(Constant(1), Constant(3)), Addition(Constant(0), Constant(5)))
+        res: int = value.numberOfParenthesisNeeded()
+        self.assertEqual(res, 2)
 
 
 if __name__ == '__main__':
