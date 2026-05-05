aa0070ccdfa5c7fa2491c4457bdd6c5f4191c00f
misProyectosPropios <inakifrutos@outlook.com>
2026-05-05 20:05:53 -0300

    fix

--- script.js
+++ script.js
@@ -279,10 +279,10 @@ res = main.obtainSolution(nums, int(${target}))
 
 }
 
-const debug = await pyodide.runPythonAsync(`
+pyodideReadyPromise.then(async (pyodide) => {
+    const debug = await pyodide.runPythonAsync(`
 import main
 dir(main)
 `);
-console.log(debug);
-
-init();
+    console.log(debug);
+});
